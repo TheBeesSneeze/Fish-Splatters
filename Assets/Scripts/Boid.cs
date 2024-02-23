c0d1bd4f3ac78e263c807e1b060e951b7cb9e6a3
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-02-22 18:37:25 -0600

    working

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -1,3 +1,4 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,36 +7,69 @@ public class Boid : MonoBehaviour
 {
     public Vector3 Velocity;
 
-    private Rigidbody rb;
+    public float NoCollisionDistance=1;
+    [Tooltip("Starting speed is random")]
+    public float MaxStartingSpeed=5;
+
+    //private Rigidbody rb;
     private Boid[] boids;
-   
+
+    private Vector3 center;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        //boids = GameObject.Find
+       RandomizeInitialVelocity();
+
+        //rb = GetComponent<Rigidbody>();
+        boids = GameObject.FindObjectsOfType<Boid>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 v1 = Rule1() * Time.deltaTime;
+        center = CalculateCenter();
 
-        Vector3 v2 = Rule2() * Time.deltaTime;
+        Vector3 v1 = Rule1();
 
-        Vector3 v3 = Rule3() * Time.deltaTime;
+        Vector3 v2 = Rule2();
 
-        rb.velocity = rb.velocity + v1 + v2 + v3;
+        Vector3 v3 = Rule3();
 
-        rb.position = rb.position + rb.velocity;
+        if (gameObject.name == "Boid" && false)
+        {
+            Debug.Log(v1);
+            Debug.Log(v2);
+            Debug.Log(v3);
+        }
+
+        Velocity = Velocity + v1 + v2 + v3;
+
+        //rb.velocity = Velocity;
+        if(!Velocity.IsNaN())
+            transform.position = (Velocity*Time.deltaTime) + transform.position;
+
+        //rb.position = rb.position + rb.velocity;
     }
 
+    private void RandomizeInitialVelocity()
+    {
+        float r1 = (Random.value - MaxStartingSpeed) * (MaxStartingSpeed*2);
+        float r2 = (Random.value - MaxStartingSpeed) * (MaxStartingSpeed*2);
+        float r3 = (Random.value - MaxStartingSpeed) * (MaxStartingSpeed*2);
+
+        Velocity = new Vector3(r1, r2, r3);
+    }
+
+    /// <summary>
+    /// mass wants to be center of flock
+    /// </summary>
+    /// <returns></returns>
     private Vector3 Rule1()
     {
-        Vector3 result = Vector3.zero;
+        Vector3 result = center;
 
-        foreach(Boid boid in boids)
+        foreach (Boid boid in boids)
         {
             if(boid != this)
             {
@@ -45,9 +79,14 @@ public class Boid : MonoBehaviour
 
         result = result / (boids.Length - 1);
 
-        return result - transform.position;
+
+        return (result - transform.position) / 100;
     }
 
+    /// <summary>
+    /// avoid collisions
+    /// </summary>
+    /// <returns></returns>
     private Vector3 Rule2()
     {
         Vector3 result = Vector3.zero;
@@ -56,33 +95,22 @@ public class Boid : MonoBehaviour
         {
             if(boid != this)
             {
-                if (Vector3.Distance(boid.transform.position, this.transform.position) < 10)
+                if (Vector3.Distance(boid.transform.position, this.transform.position) < NoCollisionDistance)
                     result = result - (boid.transform.position - this.transform.position);
             }
         }
 
         return result;
-
-        /*
-        Vector c = 0;
-
-		FOR EACH BOID b
-			IF b != bJ THEN
-				IF |b.position - bJ.position| < 100 THEN
-					c = c - (b.position - bJ.position)
-				END IF
-			END IF
-		END
-
-		RETURN c
-         */
     }
 
+    /// <summary>
+    /// average all velocities
+    /// </summary>
     private Vector3 Rule3()
     {
-        Vector3 result = Vector3.zero;
+        Vector3 result = center;
 
-        foreach(Boid boid in boids)
+        foreach (Boid boid in boids)
         {
             if(boid != this)
             {
@@ -90,8 +118,9 @@ public class Boid : MonoBehaviour
             }
         }
 
+        result = result / (boids.Length - 1);
 
-        return result;
+        return (result - Velocity) / 8f;
         /*
         Vector pvJ
 
@@ -105,5 +134,20 @@ public class Boid : MonoBehaviour
 
 		RETURN (pvJ - bJ.velocity) / 8
          */
+    }
+
+    /// <summary>
+    /// SO INEFFICIENT THIS IS CALLED ON EVERY BOID
+    /// </summary>
+    public Vector3 CalculateCenter()
+    {
+        Vector3 sum = Vector3.zero;
+
+        foreach(Boid boid in boids)
+        {
+            sum += boid.transform.position;
+        }
+
+        return sum / (boids.Length -1);
     }
 }
