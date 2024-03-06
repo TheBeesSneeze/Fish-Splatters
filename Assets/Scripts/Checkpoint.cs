e8a60c2d01adaba31e91f9a10a26d534a3e7a04f
SkyBeal <thesnugpug1@gmail.com>
2024-03-05 21:57:30 -0600

    death planes move up!!

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 //sky's workin on this one
 public class Checkpoint : MonoBehaviour
 {
+    public float DistanceFromCheckpoint = 50;
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("help");
         CheckpointManager checkpointManager = other.GetComponent<CheckpointManager>();
 
         if (checkpointManager != null)
         {
             checkpointManager.CurrentCheckpoint = transform.position;
+            MoveDeathPlane();
+        }
+    }
+
+    public void MoveDeathPlane()
+    {
+
+        Debug.Log("please");
+        DeathPlane[] deathPlanes = GameObject.FindObjectsOfType<DeathPlane>();
+
+        foreach (DeathPlane deathPlane in deathPlanes)
+        {
+            Vector3 deathPlanePosition = deathPlane.transform.position;
+            deathPlanePosition.y = transform.position.y - DistanceFromCheckpoint;
+            deathPlane.transform.position = deathPlanePosition;
         }
     }
 
